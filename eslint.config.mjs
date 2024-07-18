dad326acfc6d58b9d929236e384bf046633ae777
Hemanth Vennelakanti <hemanthvhs@gmail.com>
2024-07-17 19:40:41 -0500

    add eslint typescript setup

---
+++ eslint.config.mjs
@@ -0,0 +1,46 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default tseslint.config({
+    extends: [
+        eslint.configs.recommended,
+        ...tseslint.configs.recommendedTypeChecked,
+        eslintConfigPrettier,
+    ],
+    ignores: ['dist', 'node_modules', '*.spec.ts', 'tests/', '.github', '*.js'],
+    languageOptions: {
+        parserOptions: {
+            project: true,
+            tsconfigRootDir: import.meta.dirname,
+        },
+    },
+    rules: {
+        'no-console': 'error',
+    },
+});
+
+// export default tseslint.config(
+//     eslint.configs.recommended,
+//     ...tseslint.configs.recommendedTypeChecked,
+//     {
+//         languageOptions: {
+//             parserOptions: {
+//                 project: true,
+//                 tsconfigRootDir: import.meta.dirname,
+//             },
+//         },
+//     },
+//     eslintConfigPrettier,
+//     {
+//         rules: {
+//             // 'no-console': 'error',
+//         },
+//     },
+// );
+
+// eslintConfigPrettier should be at the last
+
+// .eslintignore is not supported in flat config as of now there we will use ignores array here.
