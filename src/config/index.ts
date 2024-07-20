adf2a8a4ec64fe3e9e99013342299da926d45209
Hemanth Vennelakanti <hemanthvhs@gmail.com>
2024-07-20 09:44:29 -0500

    add typeorm setup and datasource update and node environments setup

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,18 @@
 import { config } from 'dotenv';
+import path from 'path';
 
-config();
+// LOADING ENVIRONMENT VARIABLES BASED ON NODE_ENV
+// WE SET NODE_ENV IN PACKAGE.JSON WITH THE HELP OF CROSS-ENV PACKAGE
+config({ path: path.join(__dirname, `../../.env.${process.env.NODE_ENV}`) });
 
-const { NODE_ENV, PORT } = process.env;
+const { NODE_ENV, PORT, DB_USERNAME, DB_PASSWORD, DB_PORT, DB_DATABASE } =
+    process.env;
 
-export const Config = { NODE_ENV, PORT };
+export const Config = {
+    NODE_ENV,
+    PORT,
+    DB_USERNAME,
+    DB_PASSWORD,
+    DB_PORT,
+    DB_DATABASE,
+};
