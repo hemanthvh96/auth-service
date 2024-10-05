f685c0bf409313bc7dc8596dba6c2a90ef7e3faa
Hemanth Vennelakanti <hemanthvhs@gamyam.io>
2024-10-05 17:03:29 -0500

    add error handling and logging for register

--- src/tests/users/register.spec.ts
+++ src/tests/users/register.spec.ts
@@ -1,7 +1,26 @@
 import request from 'supertest';
 import app from '../../app';
+import { AppDataSource } from '../../config/data-source';
+import { DataSource } from 'typeorm';
+import { truncateTables } from '../utils';
+import { User } from '../../entity/User';
 
 describe('POST /auth/register', () => {
+    let connection: DataSource;
+
+    beforeAll(async () => {
+        connection = await AppDataSource.initialize();
+    });
+
+    beforeEach(async () => {
+        // TRUNCATE TABLES
+        await truncateTables(connection);
+    });
+
+    afterAll(async () => {
+        await connection.destroy();
+    });
+
     describe('Given all fields', () => {
         it('should return 201 status', async () => {
             const userData = {
@@ -33,6 +52,26 @@ describe('POST /auth/register', () => {
             expect(response.headers['content-type']).toEqual(
                 expect.stringContaining('json'),
             );
+        });
+
+        it('should persist the user in the database', async () => {
+            const userData = {
+                firstName: 'Hemanth',
+                lastName: 'V',
+                email: 'hemanthvhs@gmail.com',
+                password: 'test',
+            };
+
+            await request(app).post('/auth/register').send(userData);
+
+            const userRepository = connection.getRepository(User);
+            const users = await userRepository.find();
+
+            expect(users).toHaveLength(1);
+            expect(users[0].firstName).toBe(userData.firstName);
+            expect(users[0].lastName).toBe(userData.lastName);
+            expect(users[0].email).toBe(userData.email);
+            expect(users[0].password).toBe(userData.password);
         });
     });
 
