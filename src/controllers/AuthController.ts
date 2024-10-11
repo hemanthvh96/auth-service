f2f5507a35422123fd5f0735ce119c8206fa691b
Hemanth Vennelakanti <hvennelakanti@hawk.iit.edu>
2024-10-10 22:57:05 -0500

    add req body validations using express-validator package

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -2,6 +2,7 @@ import { NextFunction, Response } from 'express';
 import { RegisterUserRequest } from '../types';
 import { UserService } from '../services/UserService';
 import { Logger } from 'winston';
+import { validationResult } from 'express-validator';
 
 export class AuthController {
     constructor(
@@ -13,8 +14,16 @@ export class AuthController {
         res: Response,
         next: NextFunction,
     ) {
+        const result = validationResult(req);
+        if (!result.isEmpty()) {
+            // HERE I DONT NEED TO THROW ERROR
+            // BECAUSE EXRPRESS VALDIATOR ALREADY HANDLES THE ERROR DETAILS WELL
+            // THEREFORE, NOT REQUIRED TO PASS IT THROUGH OUR GLOBAL ERROR HANDLER
+            return res.status(400).json({ errors: result.array() });
+        }
         try {
             const { firstName, lastName, email, password } = req.body;
+
             this.logger.debug('New request to register a user', {
                 firstName,
                 lastName,
