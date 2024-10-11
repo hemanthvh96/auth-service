9fe0d32a931f632a85ffa33cdac0be8c897bf946
Hemanth Vennelakanti <hvennelakanti@hawk.iit.edu>
2024-10-10 19:07:58 -0500

    DB synchronize and add testcase for user role

---
+++ src/constants/index.ts
@@ -0,0 +1,6 @@
+export const Roles = {
+    CUSTOMER: 'customer',
+    ADMIN: 'admin',
+} as const;
+
+// as const make this object read only.
--- src/tests/users/register.spec.ts
+++ src/tests/users/register.spec.ts
@@ -2,8 +2,9 @@ import request from 'supertest';
 import app from '../../app';
 import { AppDataSource } from '../../config/data-source';
 import { DataSource } from 'typeorm';
-import { truncateTables } from '../utils';
+// import { truncateTables } from '../utils';
 import { User } from '../../entity/User';
+import { Roles } from '../../constants';
 
 describe('POST /auth/register', () => {
     let connection: DataSource;
@@ -14,7 +15,11 @@ describe('POST /auth/register', () => {
 
     beforeEach(async () => {
         // TRUNCATE TABLES
-        await truncateTables(connection);
+        // await truncateTables(connection);
+        // HERE THE PROBLEM WITH truncateTables is that, If we add new column in the db
+        // it wont get synchronized hence we drop the db and synchronzie the db
+        await connection.dropDatabase();
+        await connection.synchronize();
     });
 
     afterAll(async () => {
@@ -72,6 +77,25 @@ describe('POST /auth/register', () => {
             expect(users[0].lastName).toBe(userData.lastName);
             expect(users[0].email).toBe(userData.email);
             expect(users[0].password).toBe(userData.password);
+        });
+
+        it('should assign a role', async () => {
+            const userData = {
+                firstName: 'Hemanth',
+                lastName: 'V',
+                email: 'hemanthvhs@gmail.com',
+                password: 'test',
+            };
+
+            await request(app).post('/auth/register').send(userData);
+
+            const userRepository = connection.getRepository(User);
+            const users = await userRepository.find();
+
+            // expect(users[0]).toHaveProperty('role');
+            // expect(users[0].role).toBe('customer');
+
+            expect(users[0]).toHaveProperty('role', Roles.CUSTOMER);
         });
     });
 
