f6d45c6d4587e5fb0cbf856a6b415364ceec3145
Hemanth Vennelakanti <hvennelakanti@hawk.iit.edu>
2024-10-10 20:41:05 -0500

    add unique email logic and test case

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -3,16 +3,29 @@ import { User } from '../entity/User';
 import { Repository } from 'typeorm';
 import createHttpError from 'http-errors';
 import { Roles } from '../constants';
+import bcrypt from 'bcrypt';
+import logger from '../config/logger';
 
 export class UserService {
     constructor(private userRepository: Repository<User>) {}
     async create({ firstName, lastName, email, password }: UserData) {
+        const user = await this.userRepository.findOne({
+            where: { email: email },
+        });
+
+        logger.info(user);
+
+        if (user) {
+            const err = createHttpError(400, 'Email already exists!');
+            throw err;
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
         try {
             return await this.userRepository.save({
                 firstName,
                 lastName,
                 email,
-                password,
+                password: hashedPassword,
                 role: Roles.CUSTOMER,
             });
         } catch (err) {
