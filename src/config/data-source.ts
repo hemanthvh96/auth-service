228cd8c1209a70342267cdd4947c62dfeea8f4c1
Hemanth Vennelakanti <hvennelakanti@hawk.iit.edu>
2024-10-10 19:07:58 -0500

    DB synchronize and add testcase for user role

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -11,9 +11,23 @@ export const AppDataSource = new DataSource({
     password: Config.DB_PASSWORD,
     database: Config.DB_DATABASE,
     // SET synchronize: false in PRODUCTION
-    synchronize: Config.NODE_ENV === 'dev' || Config.NODE_ENV === 'test',
+    // synchronize: Config.NODE_ENV === 'dev' || Config.NODE_ENV === 'test',
+    synchronize: false,
     logging: false,
     entities: [User],
     migrations: [],
     subscribers: [],
 });
+
+/* 
+    We want synchronize to be true in dev and test but not in prod.
+    If you add a column in entity file then TypeORM will make that column synchronized 
+    with databse immediately in dev and test environments. 
+    But we dont want to do this in prod.
+
+    In prod we have to do manually.
+
+    However in the test file we use beforeAll() hook where we drop the database and synchornize
+    the database manually. Hence for safer side we are directly setting this to false.
+    So by mistake it will never be true and hence db is safe.
+*/
