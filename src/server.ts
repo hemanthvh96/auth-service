c5375d4c71346b9cd9fba053d4f0b4e61542961d
Hemanth Vennelakanti <hvennelakanti@hawk.iit.edu>
2024-11-11 22:42:04 -0600

    admin user creation

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import { Config } from './config';
 import app from './app';
 import logger from './config/logger';
 import { AppDataSource } from './config/data-source';
+import { User } from './entity/User';
 
 const startServer = async () => {
     const PORT = Config.PORT;
@@ -12,6 +13,23 @@ const startServer = async () => {
         app.listen(PORT, () => {
             logger.info(`Listening on port ${PORT}`);
         });
+
+        const adminUser = {
+            firstName: 'Master Admin F',
+            lastName: 'Master Admin L',
+            email: 'masteradmin@gmail.com',
+            password: 'masterAdmin@123',
+            role: 'admin',
+        };
+
+        const userRepository = AppDataSource.getRepository(User);
+        const admin = await userRepository.find({
+            where: { email: 'masteradmin@gmail.com' },
+        });
+        if (!admin.length) {
+            await userRepository.save(adminUser);
+            logger.info('Master Admin Created');
+        }
     } catch (err) {
         if (err instanceof Error) {
             logger.error(err.message);
